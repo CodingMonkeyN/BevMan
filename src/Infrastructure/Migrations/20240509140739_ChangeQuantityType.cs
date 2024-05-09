29517bfe5cbe4f6c88219ef9925c116070496664
Braun Nils <nils.gabriel.braun@gmail.com>
2024-05-09 16:08:00 +0200

    change quantity to int

---
+++ src/Infrastructure/Migrations/20240509140739_ChangeQuantityType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BevMan.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeQuantityType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "quantity",
+                table: "products",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "quantity",
+                table: "products",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
