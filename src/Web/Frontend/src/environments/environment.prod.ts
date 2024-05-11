f8c336eb453e74b05aec0cc6c1357bb71ad296dc
Braun Nils <nils.gabriel.braun@gmail.com>
2024-05-11 23:59:18 +0200

    add ios

--- src/Web/Frontend/src/environments/environment.prod.ts
+++ src/Web/Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://localhost:5001',
+  supabaseUrl: 'https://hqcbodgiajtbfnmuvzcq.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxY2JvZGdpYWp0YmZubXV2emNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3NTMzMTcsImV4cCI6MjAyODMyOTMxN30.JL4bkaBbpOIf9xQX15DkHBS_90L4CRnzRjKTj38EYIQ',
 };
